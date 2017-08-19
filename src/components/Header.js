d7f9bb3f42db36abfd5bc3ce9d2e0562621289b9
Fahmi Fachreza <mfahmifachreza@gmail.com>
2017-08-19 23:29:18 +0700

    update css mobile header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,10 +5,10 @@ import { Link } from 'react-router-dom';
 export default function Header() {
   return (
     <header className="container clearfix desktop-nav-wrap">
-      <div className="col lg-col-3 logowrap">
+      <div className="col sm-col-4 md-col-3 lg-col-3 logowrap">
         <h1><Link to="/">Fahmi Fachreza</Link></h1>
       </div>
-      <nav className="col lg-col-9 desktop-nav">
+      <nav className="col sm-col-8 md-col-9 lg-col-9 desktop-nav">
         <ul>
           <li><a href="https://github.com/fahmifachreza"><i className="icon icon_github" /></a></li>
           <li><a href="https://www.linkedin.com/in/mfahmif/"><i className="icon icon_linkedin" /></a></li>
