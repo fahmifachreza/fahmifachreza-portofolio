4b377860f139e51a0aee1d94fadf84fda9a4f85c
fahmifachreza <mfahmifachreza@gmail.com>
2018-03-29 17:35:28 -0400

    fix radius on button

--- src/components/Frs.js
+++ src/components/Frs.js
@@ -12,9 +12,6 @@ export default function Frs() {
             <p className="work-desc">
             Free Returns is Tokopedia’s new feature which allows buyers to return their purchased item free of charge if there is any mistake in the item received. Aside from improving Tokopedia’s shopping experience, it also allows sellers to improve the quality of their customer service. In this project I worked as a Front End Developer and UI Engineer. With the help of two colleagues, we started by developing the information architecture and setting the objective to gather 20,000 sellers to subscribe. We communicated closely with our internal division and participated in the design critique to improve the mockup. At the end we managed to finish the development process on time and conduct usability testing. As a result the project exceeds the original expectation by 200%. It successfully receives 60,000 sellers subscribe to this new feature after being launched for one week.
             </p>
-            <a href="https://www.tokopedia.com/free-returns" target="_blank" rel="noopener noreferrer" className="mb4">
-              Visit site
-            </a>
           </div>
         </div>
         <div className="col sm-col-12 md-col-12 lg-col-12 mt4 relative">
--- src/components/Topchat.js
+++ src/components/Topchat.js
@@ -2,6 +2,7 @@ import React from 'react';
 import LazyLoad from 'react-lazyload';
 import TopchatImgFull from '../assets/topchatboard.png';
 import TopChatPdf from '../assets/topchatboard.pdf';
+import resumePdf from '../assets/resume.pdf';
 // import TopchatImgFull from '../assets/dummyWireframe.png';
 
 
@@ -14,7 +15,6 @@ export default function Topchat() {
             <h2>Topchat: Tokopedia Chat Feature</h2>
             <p className="work-desc">
             Topchat is a project that is originally created during Tokopedia’s internal Hackathon Challenge. Our team aims to improve the company’s current messaging platform by developing a new design that separate contents into different threads. It can be easily used by both sellers and buyers on desktop browser and mobile application. We are currently working to expand its functionality so that the transaction and check out process can be done inside the threads.</p>
-            <a href="https://www.tokopedia.com/" rel="noopener noreferrer" target="_blank" className="mb4">Visit site</a>
           </div>
         </div>
         <div className="col sm-col-12 md-col-12 lg-col-12 mt4 relative">
